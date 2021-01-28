9b3068baef138d86258eecc2428b7f20eeb3e696
NeoFreya <neonblackcat19@gmail.com>
2021-01-28 12:58:09 +0800

    first commit

--- LoginUI/ContentView.swift
+++ LoginUI/ContentView.swift
@@ -10,7 +10,69 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Image("co_nf")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HandleView()
+                FormBox()
+            }
+        }
+    }
+}
+
+struct HandleView : View {
+    var body: some View{
+        VStack{
+            Image(systemName: "hand.draw.fill")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .background(Color.black)
+            .foregroundColor(Color.orange)
+            .padding()
+            .background(Color.black)
+            .cornerRadius(20)
+            
+            
+            Text("Hello User")
+            .foregroundColor(Color.white)
+            .padding(5)
+        }
+    }
+}
+
+struct FormBox : View {
+    @State var username : String = ""
+    @State var password : String = ""
+    
+    var body: some View{
+        VStack (alignment: .leading){
+            Text ("Username").font(.callout).bold()
+            TextField("Username", text : $username )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+            
+            Text ("Password").font(.callout).bold()
+            SecureField("Password", text: $password)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            
+            Button(action: {print("Hello Button")}) { Text("Button")
+            }
+                //  design button
+            .padding()
+            .background(Color.gray)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+            
+        }
+//        Design Vstack
+        .padding(.all, 20)
+        .background(Color.orange)
+        .cornerRadius(10)
+        
     }
 }
 
